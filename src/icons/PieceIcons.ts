c057d9c679544dff8793babd7927f78a51e1edc4
Jerrayl <jerrayl.ng@psychpress.com.au>
2022-12-14 08:01:29 +1100

    Configure typescript and add icons

---
+++ src/icons/PieceIcons.ts
@@ -0,0 +1,30 @@
+import bb from "./bb.svg";
+import bk from "./bk.svg";
+import bn from "./bn.svg";
+import bp from "./bp.svg";
+import bq from "./bq.svg";
+import br from "./br.svg";
+import wb from "./wb.svg";
+import wk from "./wk.svg";
+import wn from "./wn.svg";
+import wp from "./wp.svg";
+import wq from "./wq.svg";
+import wr from "./wr.svg";
+import blank from "./blank.svg";
+
+export const iconMap : {[key: string]: string} = {
+    "Black King" : bk,
+    "Black Queen": bq,
+    "Black Rook": br,    
+    "Black Bishop": bb,
+    "Black Knight": bn,
+    "Black Pawn": bp,    
+    "White King" : wk,
+    "White Queen": wq,
+    "White Rook": wr,    
+    "White Bishop": wb,
+    "White Knight": wn,
+    "White Pawn": wp,
+    "": blank
+}
+
