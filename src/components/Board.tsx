cf9bfb304a202a43b76a8b6b3f9d8423ee30a81a
Jerrayl <jerraylng@gmail.com>
2023-02-09 10:32:08 +1100

    Fully re-add Mobx

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,26 +1,17 @@
 import { observer } from "mobx-react";
-import { getBoardLayout, Player } from "../utils/ChessLogic";
+import { getBoardLayout } from "../utils/ChessLogic";
 import { GameStore } from "../stores/GameStore";
 import { BoardSpace } from "./BoardSpace";
-import { GameHistoryModel } from "../stores/models";
 
 export interface BoardProps {
-    currentPlayer: Player;
-    updateGameHistory: (gameHistory: GameHistoryModel | null) => void,
     gameStore: GameStore;
     classNames: string;
 }
 
-export const Board = observer(({ gameStore, currentPlayer, updateGameHistory, classNames }: BoardProps) => {
-    
-    const onSpaceClicked = (space: string) => {
-        const gameHistoryModel = gameStore.board.spaceClicked(space, currentPlayer);
-        updateGameHistory(gameHistoryModel);
-    }
-
+export const Board = observer(({ gameStore, classNames }: BoardProps) => {
     return <div className={`min-w-full grid grid-cols-8 ${classNames}`}>
         {getBoardLayout().map((coordinate) =>
-            <BoardSpace key={coordinate} onClick={() => onSpaceClicked(coordinate)} boardSpaceModel={gameStore.board.board.get(coordinate)!} />
+            <BoardSpace key={coordinate} onClick={() => gameStore.spaceClicked(coordinate)} boardSpaceModel={gameStore.board.board.get(coordinate)!} />
         )}
     </div>
 });
--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,27 +1,16 @@
-import { useState } from 'react';
+import { observer } from 'mobx-react';
 import { Board } from './Board';
 import { useStores } from '../stores/GameStore';
 import { GameHistory } from './GameHistory';
 import { CapturedPieces } from './CapturedPieces';
-import { Player } from '../utils/ChessLogic';
-import { GameHistoryModel } from '../stores/models';
 
-export const Game = () => {
+export const Game = observer(() => {
   const store = useStores();
-  const [currentPlayer, setCurrentPlayer] = useState(Player.White);
-  const [gameHistory, setGameHistory] = useState<GameHistoryModel[]>([]);
-
-  const updateGameHistory = (gameHistoryModel: GameHistoryModel | null) => {
-    const result = store.spaceClicked(gameHistory, currentPlayer, gameHistoryModel);
-    setCurrentPlayer(result.currentPlayer);
-    setGameHistory(result.gameHistory);
-  }
-
   return (
     <div className='w-full max-h-screen my-4 grid grid-flow-row md:grid-cols-12 grid-cols-6 gap-4'>
-      <CapturedPieces classNames='md:order-1 order-2  col-span-3' gameStore={store} gameHistory={gameHistory} currentPlayer={currentPlayer}/>
-      <Board classNames='md:order-2 order-1 px-4 col-span-6' gameStore={store} currentPlayer={currentPlayer} updateGameHistory={updateGameHistory} />
-      <GameHistory classNames='order-3 col-span-3' gameHistory={gameHistory} />
+      <CapturedPieces classNames='md:order-1 order-2  col-span-3' gameStore={store} />
+      <Board classNames='md:order-2 order-1 px-4 col-span-6' gameStore={store} />
+      <GameHistory classNames='order-3 col-span-3' gameHistory={store.gameHistory} />
     </div>
   );
-};
+});
